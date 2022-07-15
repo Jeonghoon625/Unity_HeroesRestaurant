8b9d5b3939e33e2c639228c5d8873cfbb638a8d7
yusoon Choi <racoonbean@gmail.com>
2022-07-15 17:28:42 +0900

    Add enemy State

--- Assets/Yusoon/Script/s1wildBoar.cs
+++ Assets/Yusoon/Script/s1wildBoar.cs
@@ -1,47 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using System;
 
+enum State
+{
+    Run,
+    Attack,
+    Dead,
+}
 public class s1wildBoar : MonoBehaviour
 {
-    public GameObject boarPrefab;
-    public int count = 3;
-    public float monsterHp = 100;
+    private State state;
     public float monsterDamage = 1;
-    public float spawnTime;
 
     private Transform target = null;
     private float moveSpeed = 2f;
 
-
-    private void Awake()
+    //public Slider sliderHp;
+    public int maxHp = 100;
+    private int currentHp;
+    public int hp
     {
-        StartCoroutine(Spawn());
+        get { return currentHp; }
+        set 
+        { 
+            currentHp = value;
+            //sliderHp.value = currentHp / maxHp;
+        }
     }
 
-    IEnumerator Spawn()
+    public Animator anim;
+
+    private void Start()
     {
-        for(int i = 0; i < count; i++)
-        {
-            Vector3 pos = transform.position;
-            pos.x = 0.5f * i;
-            transform.position = pos;
-            Instantiate(boarPrefab, pos, Quaternion.identity);
-        }
-        yield return new WaitForSeconds(spawnTime);
+        currentHp = maxHp;
+        //sliderHp.maxValue = maxHp;
+        //sliderHp.value = currentHp;
     }
 
     private void Update()
+    {
+        if (state == State.Run)
+        {
+            UpdateRun();
+        }
+        else if (state == State.Attack)
+        {
+            UpdateAttack();
+        }
+        else if (state == State.Dead)
+        {
+            UpdateDead();
+        }
+    }
+
+    private void UpdateRun()
     {
         if (target != null)
         {
             Vector3 dir = target.position - transform.position;
             transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
         }
+        state = State.Run;
+        anim.SetTrigger("Run");
     }
+
+    private void UpdateAttack()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if(distance <= 2)
+        {
+            state = State.Attack;
+            anim.SetTrigger("Attack");
+        }
+
+    }
+
+    private void UpdateDead()
+    {
+        if(currentHp <= 0)
+        {
+            currentHp = 0;
+            state = State.Dead;
+            anim.SetTrigger("Dead");
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Hero")
         {
             target = other.gameObject.transform;
         }
