d860d859438242c9c732525c96ca5ebe669cc319
Jeonghoon625 <dr.kim1997@gmail.com>
2022-07-18 17:31:31 +0900

    Add Game Manager and SceneLoader

---
+++ Assets/Main/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+    public static void LoadScene(string sceneName="")
+    {
+        if(sceneName == "")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
