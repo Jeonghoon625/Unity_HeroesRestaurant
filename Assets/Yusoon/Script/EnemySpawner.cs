9e21a4d990481db6a448393bd644b88148dd758a
yusoon Choi <racoonbean@gmail.com>
2022-07-18 15:27:38 +0900

    1-1 wave 구현

--- Assets/Yusoon/Script/EnemySpawner.cs
+++ Assets/Yusoon/Script/EnemySpawner.cs
@@ -11,34 +11,57 @@ public class EnemySpawner : MonoBehaviour
     private int currentWaveNumber;
 
     private int monsterRemains;
-    private float nextSpawnTime;
+    private int enemyRemainingAlive;
 
     private void Start()
     {
         NextWave();
     }
-    private void Update()
-    {
-        if(monsterRemains > 0 &&Time.time > nextSpawnTime)
-        {
-            monsterRemains--;
-            nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
 
-            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+    private void OnEnemyDeath()
+    {
+        enemyRemainingAlive--;
+
+        if(enemyRemainingAlive == 0)
+        {
+            NextWave();
         }
     }
+
     private void NextWave()
     {
         currentWaveNumber++;
-        currentWave = waves[currentWaveNumber - 1];
+       
+        if (currentWaveNumber - 1 < waves.Length)
+        {
+            currentWave = waves[currentWaveNumber - 1];
 
-        monsterRemains = currentWave.monsterCount;
+            monsterRemains = currentWave.enemys.Count;
+            enemyRemainingAlive = monsterRemains;
+
+            for(int i = 0; i < monsterRemains; i++)
+            {
+                var pos = currentWave.pos[i];
+                Quaternion rot = Quaternion.identity;
+                if(pos.x > 0)
+                {
+                    rot.y = 180f;
+                }
+                else
+                {
+                    rot.y = 0f;
+                }
+
+                Enemy spawnedEnemy = Instantiate(currentWave.enemys[i], pos, rot);
+                spawnedEnemy.OnDeath += OnEnemyDeath;
+            }
+        }
     }
 
     [System.Serializable]
    public class Wave
     {
-        public int monsterCount;
-        public float timeBetweenSpawns;
+        public List<Enemy> enemys = new List<Enemy>();
+        public List<Vector3> pos = new List<Vector3>();
     }
 }
