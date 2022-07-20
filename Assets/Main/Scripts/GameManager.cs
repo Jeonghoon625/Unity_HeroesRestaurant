04fb3df29cd37c468b48464de32179a08969d4a9
yusoon Choi <racoonbean@gmail.com>
2022-07-20 15:56:36 +0900

    gameManager MasterStage, slaveStage add

--- Assets/Main/Scripts/GameManager.cs
+++ Assets/Main/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     int test = 625;
+    public int masterStage = 1;
+    public List<int> slaveStage = new List<int>();
+
     public GameManager()
     {
         Debug.Log("게임 매니저 초기화");
