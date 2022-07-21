28bb94db00ec761d4c52aaea66b620f75a3f2a88
Ha Jung Song <hajungs94@gmail.com>
2022-07-22 01:59:05 +0900

    Fix error Building data

--- Assets/SongHaJung/Script/BuildingInfo.cs
+++ Assets/SongHaJung/Script/BuildingInfo.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BuildingInfo
 {
-    public int activeselectionIndex;
-    public float positionX;
-    public bool Istrue;
+    //public int activeselectionIndex;
+    //public float positionX;
+    //public bool Istrue;
 
-    public BuildingInfo(int _activeselectionIndex, float _positionX = 0, bool _Istrue)
-    {
-        this.activeselectionIndex = _activeselectionIndex;
-        this.positionX = _positionX;
-        this.Istrue = _Istrue;
-    }
+    //public BuildingInfo(int _activeselectionIndex, float _positionX = 0, bool _Istrue)
+    //{
+    //    this.activeselectionIndex = _activeselectionIndex;
+    //    this.positionX = _positionX;
+    //    this.Istrue = _Istrue;
+    //}
 }
