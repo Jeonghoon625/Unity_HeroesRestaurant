cfb99497989c7a69b896e587e255cb95bc89d6c1
Kimkyeungsoo <96224@naver.com>
2022-07-25 13:32:18 +0900

    클리어 보상 및 강화

---
+++ Assets/Gang/Scripts/Reward.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum ClearReward
+{
+    meat = 1,
+    mushroom = 2,
+    fish = 3,
+}
+public class Reward : MonoBehaviour
+{
+    public GameObject rewardSlot;
+    public GameObject[] prefabs;
+
+    public void ClearReward(int prefabNum, int count)
+    {
+        var re = Instantiate(prefabs[prefabNum - 1]);
+        re.transform.SetParent(rewardSlot.transform, false);
+        re.GetComponentInChildren<TextMeshProUGUI>().text = $"{count}";
+    }
+}
