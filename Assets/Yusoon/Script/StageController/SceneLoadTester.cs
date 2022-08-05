92263144f97548c658609cdced9684293a7e0878
Jeonghoon625 <dr.kim1997@gmail.com>
2022-08-05 17:31:43 +0900

    Amend Title Scene(Add Time Period)

--- Assets/Yusoon/Script/StageController/SceneLoadTester.cs
+++ Assets/Yusoon/Script/StageController/SceneLoadTester.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneLoadTester : MonoBehaviour
 {
+    private bool isTouch = false;
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !isTouch)
         {
-            SceneManager.LoadScene("LoadingScene");
+            isTouch = true;
+            StartCoroutine(timePeriod());
         }
+    }
+
+    IEnumerator timePeriod()
+    {
+        float timer = 0;
+
+        while(timer > 1f)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("LoadingScene");
     }
 }
